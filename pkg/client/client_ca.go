23bb9063dea8df5f8b27e26447576486c66e5cbe
Antonio Fdez <antoniofernandezvara@gmail.com>
2021-02-01 07:55:18 +0100

    test: added tests for client

--- pkg/client/client_ca.go
+++ pkg/client/client_ca.go
@@ -1,9 +1,23 @@
 package client
 
+import (
+	"net/http"
+)
+
 // CACreate returns status information if connection is ok to the API, error otherwise
 func (c *Client) CACreate(request APICertificateRequest) (response Certificate, err error) {
 
-	_, err = c.http.Post("/v1/ca").BodyJSON(request).ReceiveSuccess(&response)
+	var (
+		res *http.Response
+	)
+
+	res, err = c.http.Post("/v1/ca").BodyJSON(request).ReceiveSuccess(&response)
+	if err != nil {
+		return
+	}
+
+	err = isError(res, http.StatusCreated)
+
 	return
 
 }
--- pkg/client/client_get.go
+++ pkg/client/client_get.go
@@ -1,19 +1,29 @@
 package client
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 // CertificateGet returns the certificate information if found
 func (c *Client) CertificateGet(caID, cn string, remaining int) (response Certificate, err error) {
 
 	var (
 		uri string = fmt.Sprintf("/v1/ca/%s/certificates/%s", caID, cn)
+		res *http.Response
 	)
 
 	if remaining > 0 {
 		uri = fmt.Sprintf("%s?renew=%d", uri, remaining)
 	}
 
-	_, err = c.http.Get(uri).ReceiveSuccess(&response)
+	res, err = c.http.Get(uri).ReceiveSuccess(&response)
+	if err != nil {
+		return
+	}
+
+	err = isError(res, http.StatusOK)
+
 	return
 
 }
